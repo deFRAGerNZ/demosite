68d6b57c965fd114aec90f941826d09976012db9
watsonrp <watsonrp@cpit.ac.nz>
2013-11-30 19:59:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<h1>Placeholder page</h1>
 <?php
-print "testing";
+
+$headers = apache_request_headers();
+
+foreach ($headers as $header => $value) {
+    echo "$header: $value <br />\n";
+}
+
 ?>
